7763235be32705909e7f5cab84687b2842897de5
mohamadaffouri <mohamad.aff@gmail.com>
2025-04-10 15:53:06 +0300

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Affouri Test Hello ooooo ooooo oooooo ooooo oooo Laravel</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
